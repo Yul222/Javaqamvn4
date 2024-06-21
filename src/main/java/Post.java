51a508039a2f0f393c96ceb07e76682121baf4db
Yul <montazstroy@inbox.ru>
2024-06-21 08:21:54 +0300

    add classes

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // Имя пользователя
+    public String passport; // Паспортные данные
+    public String patronymic; // Отчество
+    public String phone; // Телефон
+    public String surname; // Фамилия
+    public boolean subscription; // Подписка на новости
+    public  FormDate birthday;
+
+}
